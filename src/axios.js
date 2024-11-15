0681eca3e3595132a980e439b7411bf8d6dbdb5c
sreyamanoharan <sreyampk@gmail.com>
2024-11-15 11:02:58 +0530

    link

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://booknest-back.onrender.com/",
+  
 });
 
 export default instance;  
